b43d907172a86452dbd5f95166752297109ec6c0
Ferdi <ferdikt@gmail.com>
2024-09-26 16:10:24 +0300

    Initial Commit

---
+++ swiftui-component-gallery/swiftui_component_galleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_component_galleryApp.swift
+//  swiftui-component-gallery
+//
+//  Created by Ferdi Kiziltoprak on 26.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_component_galleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
